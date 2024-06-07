dad2a45971f1777ef07c6857f5b279e5a139f7b4
LuisElianM <luiselian001@gmail.com>
2024-06-06 23:40:36 -0400

    feat(domain): :sparkles: Ingredient domain layer implemented
    Domain layer with the ingredient entity and repository

---
+++ src/apps/ingredient/domain/repositories/ingredient_repository.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod 
+from ..ingredient import Ingredient
+
+class IngredientRepository(ABC):
+    @abstractmethod
+    def find_all_ingredients(self):
+        pass
+
+    @abstractmethod
+    def find_ingredient_by_id(self, id: str):
+        pass
+
+    @abstractmethod
+    def save_ingredient(self, ingredient: Ingredient):
+        pass
+
+    @abstractmethod
+    def delete_ingredient(self, ingredient: Ingredient):
+        pass
+
+    @abstractmethod
+    def update_ingredient(self, ingredient: Ingredient):
+        pass
