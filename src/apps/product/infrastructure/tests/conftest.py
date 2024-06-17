575d180ec3c4b95f97f90ee9241ec3e322d3486f
Georopeza <149003391+Georopeza@users.noreply.github.com>
2024-06-17 02:53:22 +0000

    Se borró la carpeta tests del nivel de in frastructure

--- src/apps/product/infrastructure/tests/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from starlette.testclient import TestClient
-from ....main import app 
-
-@pytest.fixture(scope="module")
-def test_app():
-    client = TestClient(app)
-    yield client  # esto proporcionará el cliente de prueba en las pruebas
