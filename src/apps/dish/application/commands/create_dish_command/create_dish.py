6e139e151ba27db668b936438bfee55cc458596c
Joyce <joywg14@gmail.com>
2024-06-23 16:03:18 -0400

    feat(application): :sparkles: add ingredients to the dish without completing

--- src/apps/dish/application/commands/create_dish_command/create_dish.py
+++ src/apps/dish/application/commands/create_dish_command/create_dish.py
@@ -1,6 +1,8 @@
 from ....domain import Dish, DishRepository
+from ...exceptions.no_ingredients_in_stock import NoIngredientInStockException
 from .types import CreateDishDto
 from core.application.services.application_service import Service
+from .....ingredient.application.queries.get_ingredient_by_id_query.get_ingredient_by_id import GetIngredientByIdService
 from core.application.results.result import Result
 from core.infrastructure.providers.uuid_service import UUIDService
 
@@ -13,10 +15,25 @@ class CreateDishService(Service[CreateDishDto, str]):
 
     def execute(self, data: CreateDishDto) -> Result[str]:
         _id = self.idGenerator.generate_id()
+        all_ingredients = GetIngredientByIdService()
+        
+        for ingredient in data.ingredients:
+            ingredients = all_ingredients.execute(ingredient)
+            
+            if  ingredients.is_failure():
+                return Result[str].make_failure(error=NoIngredientInStockException())
         #TODO:
-        #data.ingredients -> Esta es la lista de ids de los ingredientes 
-        #Agregar logica de validacion de existencia de ingredientes
-        #Si no se tienen todos los ingredientes, no se crea el plato
+        #Hacer un update por cada uno de los ingredientes que existan en el plato
+        #donde se le reste uno a la cantidad para quitarlo del inventario
+        #rellenar con esos datos la tabla de Ingrediente-plato 
+        
+        # ingredient.quantity -=1
+        # if ingredient.quantity < 0:
+        #     return Result[str].make_failure(error=NoIngredientInStockException())
+    
+        # self.Ingredient_dish_repository.update_ingredient(ingredient)
+        # self.Ingredient_dish_repository.add_ingredient_to_dish(ingredient, _id)
+        
         dish = Dish(_id, data.name, data.description, data.price, data.disponibility)
         self.dish_repository.save_dish(dish)
 
